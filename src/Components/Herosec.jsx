6b1f0e3a24d18a3e46a259bc86a9cca482508cd0
Ratul-Raha <ratulraha.04@gmail.com>
2024-10-09 13:12:11 +0600

    Navbar and hero section half commplete here

--- src/Components/Herosec.jsx
+++ src/Components/Herosec.jsx
@@ -7,6 +7,21 @@ const Herosec = () => {
             <div id="herosection">
                 <div className="container-fluid">
                     <Navbar/>
+
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-12">
+                               <div className="title-wrap">
+                                 <h1>Discover.Learn.Enjoy</h1>
+                                 <span>Platform for creatives around the world </span>
+                                  <div className="search-wrap">
+                                  <input type="search" placeholder="Search anything" />
+                                  <button className="btn"><span>search</span></button>
+                                 </div>
+                               </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
